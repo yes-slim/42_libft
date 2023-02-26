56e738e25f59fb4d59dac0c55c0e980aa4dd6c0c
乃ㄥ4乙3_ㄚ <99927294+BL4Z3-y@users.noreply.github.com>
2023-02-26 18:30:56 +0100

    push

--- Srcs/ft_strjoin.c
+++ Srcs/ft_strjoin.c
@@ -26,18 +26,26 @@ static int	join(int i, int j, char *str, char const *s)
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	int		i;
+	char	*new;
 	int		j;
-	char	*str;
 
-	if (s1 == NULL || s2 == NULL)
-		return (ft_strdup(""));
-	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
-	if (!str)
+	if (!s2)
 		return (NULL);
-	j = 0;
+	if (!s1)
+		return (ft_strdup(s2));
 	i = 0;
-	j = join(i, j, str, s1);
-	j = join(i, j, str, s2);
-	str[j] = '\0';
-	return (str);
+	j = ft_strlen(s1);
+	new = malloc(ft_strlen(s1) + ft_strlen(s2) * sizeof(char) + 1);
+	if (!new)
+		return (NULL);
+	while (s1[i])
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i])
+		new[j++] = s2[i++];
+	new[j] = '\0';
+	return (new);
 }
